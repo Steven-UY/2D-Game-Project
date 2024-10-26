247a48533bcda7a7f049160042a51440e2eb9459
kjannath <137518400+kjannath@users.noreply.github.com>
2024-10-26 12:07:12 -0400

    sawshark movement

---
+++ vgd-lab-3/Scripts/sawshark.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+var speed = 200
+var direction: int = 1  # 1 for right, -1 for left
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	# Update horizontal position directly
+	position.x += direction * speed * delta
+
+	# Flip the sprite based on direction
+	
+	# Debug output
+	# print("Fish Position: ", position)
+
+	# Edge detection
+	if position.x < 0 or position.x > get_viewport_rect().size.x:
+		direction *= -1  # Reverse direction
