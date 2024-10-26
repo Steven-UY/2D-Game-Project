29662046878534cd1fc84c8b3ab09911310e7d28
TianyiFan2003 <tf3@fordham.edu>
2024-10-26 00:10:11 -0400

    chest change

--- vgd-lab-3/Scenes/chest.gd
+++ vgd-lab-3/Scenes/chest.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
-
+var is_open = false
+# maincharacter.gd
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	$AnimatedSprite2D.play('default')
+	add_to_group("Player")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -12,4 +13,7 @@ func _process(delta: float) -> void:
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	$AnimatedSprite2D.play('Open')
+	$AnimatedSprite2D.play('open')
+	if not is_open and body.is_in_group("Player"): 
+		is_open = true  
+		$AnimatedSprite2D.play('opened')
