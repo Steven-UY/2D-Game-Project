7a765db6dac5371810e8ad166f405cd03a04defe
Steven Uy <81347262+Steven-UY@users.noreply.github.com>
2024-10-28 19:12:24 -0400

    healthbar responds made changes to redfish(in main tho I think?)

--- vgd-lab-3/Scripts/maincharacter.gd
+++ vgd-lab-3/Scripts/maincharacter.gd
@@ -1,19 +1,27 @@
 #inherits from CharacterBody2D class
 extends CharacterBody2D
 
-# Reference to the PlayerAnimation node
-@onready var player_animation = $PlayerAnimation
+@onready var player_animation = $PlayerAnimation # Reference to the PlayerAnimation node
 @export var rotation_speed: float = TAU * 2
 @onready var collision_shape = $CollisionShape2D  # Reference to CollisionShape2D node
+@onready var health_bar =  $"../CanvasLayer/ProgressBar"
 
 # Constants for movement
 const SWIM_SPEED = 1000  # Adjust this value to make swimming slower
 const DRAG = 0.9  # Simulates water resistance (higher value means less inertia)
 const BUOYANCY = -30  # Buoyancy to slowly pull player upward when idle
+var HP = 10
+
+
 
 func _ready():
 	# Automatically play the swim animation when the player spawns
 	player_animation.play("swim_up")  # Replace "swim_up" with your animation name
+	health_bar.value = HP
+	
+func healthDown():
+	HP -= 1
+	health_bar.value = HP
 
 #delta parameter allows behaviour to be unchanged with framerate(check docs)
 func _physics_process(delta):
@@ -34,11 +42,12 @@ func _physics_process(delta):
 		
 # Update collision shape rotation to match the character's rotation
 	collision_shape.rotation_degrees = rotation_degrees
+	
 
 	# Normalize input direction to prevent diagonal speed boost(check docs)
 	if input_direction.length() > 0:
 		input_direction = input_direction.normalized()
-
+		
 	# Apply movement with inertia
 	velocity += input_direction * SWIM_SPEED * delta
 
@@ -51,3 +60,8 @@ func _physics_process(delta):
 
 	# Move the character
 	move_and_slide()
+	
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	healthDown()
