a802be41bd993507e25a81345879e8248f625e52
TianyiFan2003 <tf3@fordham.edu>
2024-10-29 14:45:49 -0400

    coins

--- vgd-lab-3/GoldCoin.gd
+++ vgd-lab-3/GoldCoin.gd
@@ -12,4 +12,5 @@ func _process(delta: float) -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group('Player'):
-		queue_free()
+		pass
+		#queue_free()
--- vgd-lab-3/Scenes/chest.gd
+++ vgd-lab-3/Scenes/chest.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+const GOLD_COIN = preload("res://Scenes/GoldCoin.tscn")
 var is_open = false
 # maincharacter.gd
 # Called when the node enters the scene tree for the first time.
@@ -17,3 +17,12 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if not is_open and body.is_in_group("Player"): 
 		is_open = true  
 		$AnimatedSprite2D.play('open')
+		
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if $AnimatedSprite2D.animation =="open":
+		var newcoin = GOLD_COIN.instantiate()
+		print("coin")
+		#add_child(newcoin)
+		call_deferred("add_child", newcoin)
