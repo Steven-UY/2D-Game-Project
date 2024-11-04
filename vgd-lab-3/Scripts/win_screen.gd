51dfffbad1503d091b44bc831a8620526e9e7c1f
kjannath <137518400+kjannath@users.noreply.github.com>
2024-11-04 11:09:11 -0500

    polishing

--- vgd-lab-3/Scripts/win_screen.gd
+++ vgd-lab-3/Scripts/win_screen.gd
@@ -13,3 +13,7 @@ func _process(delta: float) -> void:
 func _on_button_pressed() -> void:
 	Global.reset()
 	get_tree().reload_current_scene()
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/mainMenu.tscn")
